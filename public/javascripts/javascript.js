dc0881185c286b8136f79bc47797ed9d5668e4c8
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2019-06-08 21:38:13 +0300

    Remade the top menu with flex.
    Top menu with flexbox styling. Added a few drop-down placeholders with connected data-pages. Tried to omit the drop-triggers out of the nodeList collection but couldn't; still, managed to have them not throw errors on click.

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -1,16 +1,21 @@
 
 function initTopMenu() {	// find all top-menu items and handle their "onclick" events.
-	var links = document.querySelectorAll("#topMenu a");
+	const links = document.querySelectorAll("#topMenu a");
 	console.info(links);
 
 	for(var i = 0; i < links.length; i++) {
-		links[i].onclick = clickTmItem;	// no "()" after the function name so the browser calls it not us.
+		if (links[i].hasAttribute("data-page")) {	// don't collect irrelevant anchors.
+			links[i].onclick = clickTmItem;	// no "()" after the function name so the browser calls it not us.
+		} else {
+			continue;
+		}
 	}
 }
 
 function clickTmItem() {	// Click Top Menu Item.
 	console.warn("clicked on menu", this);	// "this" is a variable which was just used. Here: just clicked on.
 	hidePages();
+	
 	var pageId = this.getAttribute("data-page");
 	console.warn({pageId});
 	showPage(pageId);
