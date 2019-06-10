52ba979e702288646ec6ae3540a6ca4196e8c1e7
PopanIonut <ionut.popan@yahoo.com>
2019-06-10 21:20:45 +0300

    add json from local

--- public/javascripts/javascript.js
+++ public/javascripts/javascript.js
@@ -3,7 +3,7 @@ function initTopMenu() {	// find all top-menu items and handle their "onclick" e
 	const links = document.querySelectorAll("#topMenu a");
 	console.info(links);
 
-	for(var i = 0; i < links.length; i++) {
+	for (var i = 0; i < links.length; i++) {
 		if (links[i].hasAttribute("data-page")) {	// don't collect irrelevant anchors.
 			links[i].onclick = clickTmItem;	// no "()" after the function name so the browser calls it not us.
 		} else {
@@ -15,22 +15,53 @@ function initTopMenu() {	// find all top-menu items and handle their "onclick" e
 function clickTmItem() {	// Click Top Menu Item.
 	console.warn("clicked on menu", this);	// "this" is a variable which was just used. Here: just clicked on.
 	hidePages();
-	
+
 	var pageId = this.getAttribute("data-page");
-	console.warn({pageId});
+	console.warn({ pageId });
 	showPage(pageId);
 }
 
 function hidePages() {
 	var pages = document.querySelectorAll(".page");
-	pages.forEach(function(page) {
+	pages.forEach(function (page) {
 		page.style.display = "none";
 	});
 }
 
 function showPage(page) {	// sets the visibility status of the ID'd object; none = hidden, block = visible.
-    console.warn(page);
-    document.getElementById(page).style.display = "block";
+	console.warn(page);
+	document.getElementById(page).style.display = "block";
 }
+
+
+
+function display(persons) {
+	var list = persons.map(function (person) {
+		return `<tr data-id="${person.town}">
+            <td>${person.adress}</td>
+            <td>${person.from}</td>
+            <td>${person.until}</td>
+            </tr>`;
+	});
+	document.querySelector('#addresses tbody').innerHTML = list.join('');
+}
+
+
+var API_URL = {
+	ADD: 'data/parkingData.json',
+	READ: 'data/parkingData.json',
+  };
+  
+  var API_METHOD = {
+	ADD: 'POST'
+  };
+  
+  fetch(API_URL.READ).then(function(r){
+	return r.json()
+  }).then(function(parkingData){
+	allPersons = parkingData;
+	display(parkingData);
+  })
+  
 
 initTopMenu();
