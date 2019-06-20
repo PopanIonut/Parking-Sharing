85310dfe958219b8381d4df1d6560cb22dc9b3ee
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2019-06-21 00:55:13 +0300

    DB: READ ok.
    TODO: create, update, delete  records.

--- public/data/parking.sql
+++ public/data/parking.sql
@@ -6,11 +6,11 @@ CREATE TABLE spots
 	id INT unsigned NOT NULL auto_increment,
 	city_town VARCHAR(50) NOT NULL,		# City or town name where the parking spot is.
 	str_address VARCHAR(255) NOT NULL,	# Street address & spot ID of the alotted parking spot.
-	space_nr VARCHAR(10) NOT NULL,		# Lawful id. number of the alotted parking spot.
+	spot_nr VARCHAR(10) NOT NULL,		# Lawful id. number of the alotted parking spot.
 	t_from datetime NOT NULL,
   	t_until datetime NOT NULL,
 
-# ?? separate the date & time fields from this as into a table of their own ??
+# ?? separate the date & time fields from this into a table of their own ??
 
 	PRIMARY KEY (id)
 	
