baf416c09bfd5e3cf57857a0852e592c96a5d2aa
V. Zsombor <47913849+Zsombi55@users.noreply.github.com>
2019-06-21 00:57:08 +0300

    comment typo

--- public/data/parking.sql
+++ public/data/parking.sql
@@ -5,7 +5,7 @@ CREATE TABLE spots
 (
 	id INT unsigned NOT NULL auto_increment,
 	city_town VARCHAR(50) NOT NULL,		# City or town name where the parking spot is.
-	str_address VARCHAR(255) NOT NULL,	# Street address & spot ID of the alotted parking spot.
+	str_address VARCHAR(255) NOT NULL,	# Street address of the alotted parking spot.
 	spot_nr VARCHAR(10) NOT NULL,		# Lawful id. number of the alotted parking spot.
 	t_from datetime NOT NULL,
   	t_until datetime NOT NULL,
