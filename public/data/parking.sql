2dc38797224169577b94cfa6cc7d7a0b79ba7ca7
PopanIonut <ionut.popan@yahoo.com>
2019-06-24 21:47:03 +0300

    db schema & data example

--- public/data/parking.sql
+++ public/data/parking.sql
@@ -1,18 +1,155 @@
-USE parking;
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 24, 2019 at 08:41 PM
+-- Server version: 10.1.40-MariaDB
+-- PHP Version: 7.3.5
 
-DROP TABLE IF EXISTS spots;
-CREATE TABLE spots
-(
-	id INT unsigned NOT NULL auto_increment,
-	city_town VARCHAR(50) NOT NULL,		# City or town name where the parking spot is.
-	str_address VARCHAR(255) NOT NULL,	# Street address of the alotted parking spot.
-	spot_nr VARCHAR(10) NOT NULL,		# Lawful id. number of the alotted parking spot.
-	t_from datetime NOT NULL,
-  	t_until datetime NOT NULL,
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-# ?? separate the date & time fields from this into a table of their own ??
 
-	PRIMARY KEY (id)
-	
-	# ENGINE=InnoDB DEFAULT CHARSET=utf8_general_ci.
-); 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `parking`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `people`
+--
+
+CREATE TABLE `people` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `phone` int(20) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `car_nr` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `people`
+--
+
+INSERT INTO `people` (`id`, `firstName`, `lastName`, `phone`, `email`, `car_nr`) VALUES
+(1, 'Ioan', 'Marcel', 23452, 'fdgdhdg@dfgd', 'Cj345fg'),
+(2, 'Maria', 'Bob', 35644, 'dfgdhf', 'cj3435gf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservations`
+--
+
+CREATE TABLE `reservations` (
+  `id` int(11) NOT NULL,
+  `person_id` int(11) NOT NULL,
+  `spot_id` int(11) NOT NULL,
+  `start` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ending` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `person_id`, `spot_id`, `start`, `ending`) VALUES
+(1, 1, 1, '2019-06-24 18:35:24', '0000-00-00 00:00:00'),
+(2, 2, 1, '2019-06-24 18:36:13', '0000-00-00 00:00:00'),
+(3, 2, 1, '2019-06-24 18:37:16', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spots`
+--
+
+CREATE TABLE `spots` (
+  `id` int(11) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `area` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `spot_nr` varchar(30) NOT NULL,
+  `t_from` time NOT NULL,
+  `t_until` time NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `spots`
+--
+
+INSERT INTO `spots` (`id`, `city`, `area`, `address`, `spot_nr`, `t_from`, `t_until`, `description`) VALUES
+(1, 'Cluj', 'Zorilor', 'zorilor 24', 'A2', '00:00:22', '00:00:23', 'dgfhj');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `people`
+--
+ALTER TABLE `people`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `person_id` (`person_id`),
+  ADD KEY `spot_id` (`spot_id`);
+
+--
+-- Indexes for table `spots`
+--
+ALTER TABLE `spots`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `people`
+--
+ALTER TABLE `people`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reservations`
+--
+ALTER TABLE `reservations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `spots`
+--
+ALTER TABLE `spots`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `reservations`
+--
+ALTER TABLE `reservations`
+  ADD CONSTRAINT `reservations_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `people` (`id`),
+  ADD CONSTRAINT `reservations_ibfk_2` FOREIGN KEY (`spot_id`) REFERENCES `spots` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
